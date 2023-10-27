db7a2a5653c80603cbcc0444a71f3dd9952ef3a6
parth5757 <psthakkar2@gmail.com>
2023-10-27 22:41:40 +0530

    27/10/23 1

--- frontend/src/components/sidebar/sidebar.jsx
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -1,4 +1,16 @@
 import "./sidebar.scss"
+import GridViewIcon from '@mui/icons-material/GridView';
+import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+import DvrIcon from '@mui/icons-material/Dvr';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import SettingsSystemDaydreamIcon from '@mui/icons-material/SettingsSystemDaydream';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Sidebar = () => {
     return (
@@ -8,18 +20,61 @@ const Sidebar = () => {
             </div>
             <hr />
             <div className="center">
-                <li>
-                    <span>Dashboard</span>
-                </li>
-                <li>
-                    <span>Dashboard</span>
-                </li>
-                <li>
-                    <span>Dashboard</span>
-                </li>
-                <li>
-                    <span>Dashboard</span>
-                </li>
+                <ul>
+                    <p className="title">Main</p>
+                    <li>
+                    <GridViewIcon />
+                        <span>Dashboard</span>
+                    </li>
+                    <p className="title">LISTS</p>
+                    <li>
+                        <SupervisedUserCircleIcon  />
+                        <span>Users</span>
+                    </li>
+                    <li>
+                        <ProductionQuantityLimitsIcon />
+                        <span>Products</span>
+                    </li>
+                    <li>
+                        <DvrIcon />
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <LocalShippingIcon />
+                        <span>Delivery</span>
+                    </li>
+                    <p className="title">Useful</p>
+                    <li>
+                        <QueryStatsIcon />
+                        <span>Stats</span>
+                    </li>
+                    <li>
+                        <NotificationsActiveIcon />
+                        <span>Notification</span>
+                    </li>
+                    <p className="title">SERVICE</p>
+                    <li>
+                        <SettingsSystemDaydreamIcon />
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                        <VpnKeyIcon />
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                        <SettingsIcon />
+                        <span>Settings</span>
+                    </li>
+                    <p className="title">User</p>
+                    <li>
+                        <AssignmentIndIcon />
+                        <span>Profiles</span>
+                    </li>
+                    <li>
+                        <LogoutIcon />
+                        <span>Log Out</span>
+                    </li>
+                </ul>
             </div>
             <div className="bottom">Color option</div>
             
